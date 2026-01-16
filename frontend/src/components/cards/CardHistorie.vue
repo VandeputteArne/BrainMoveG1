7a50bcbe57305984c356c7e26bb937bff92064f7
JonathanMat <jonathan.matthys@student.howest.be>
2026-01-16 15:34:07 +0100

    feat: enhance AppHistorie view with CardHistorie component and display results

---
+++ frontend/src/components/cards/CardHistorie.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { History, ChevronRight } from 'lucide-vue-next';
+
+const props = defineProps({
+  gebruiker: {
+    type: String,
+    required: true,
+  },
+  datumtijd: {
+    type: String,
+    required: true,
+  },
+  gemtijd: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <router-link class="c-card-historie" :to="props.url">
+    <History class="c-card-historie__icon" />
+    <div class="c-card-historie__info">
+      <p>{{ props.gebruiker }}</p>
+      <p class="small">{{ props.datumtijd }}</p>
+    </div>
+    <div class="c-card-historie__score">
+      <p class="small">Gem Tijd</p>
+      <h3>{{ props.gemtijd }}</h3>
+    </div>
+    <ChevronRight class="c-card-historie__icon" />
+  </router-link>
+</template>
+
+<style scoped></style>
--- frontend/src/views/historie/AppHistorie.vue
+++ frontend/src/views/historie/AppHistorie.vue
@@ -2,6 +2,7 @@
 import FilterGame from '../../components/filters/FilterGame.vue';
 import InputGebruikersnaam from '../../components/inputs/InputGebruikersnaam.vue';
 import FilterDatum from '../../components/filters/FilterDatum.vue';
+import CardHistorie from '../../components/cards/CardHistorie.vue';
 </script>
 
 <template>
@@ -16,7 +17,13 @@ import FilterDatum from '../../components/filters/FilterDatum.vue';
     <div class="c-historie__overzicht">
       <h3>Color Sprint</h3>
       <p>Alle Resultaten</p>
-      <div class="c-historie__resultaten"></div>
+      <div class="c-historie__resultaten">
+        <CardHistorie gebruiker="Jan Jansen" datumtijd="12-03-2024 14:30" gemtijd="00:45" score="1500" url="/resultaten/1/proficiat" />
+        <CardHistorie gebruiker="Piet Pietersen" datumtijd="10-03-2024 16:00" gemtijd="00:50" score="1400" url="/resultaten/2/proficiat" />
+        <CardHistorie gebruiker="Klaas Klaassen" datumtijd="08-03-2024 10:15" gemtijd="00:55" score="1300" url="/resultaten/3/proficiat" />
+        <CardHistorie gebruiker="Marie de Vries" datumtijd="05-03-2024 09:45" gemtijd="00:48" score="1450" url="/resultaten/4/proficiat" />
+        <CardHistorie gebruiker="Sophie van den Berg" datumtijd="03-03-2024 11:20" gemtijd="00:52" score="1350" url="/resultaten/5/proficiat" />
+      </div>
     </div>
   </div>
 </template>
